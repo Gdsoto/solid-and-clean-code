ca43c72a3f9e0db21c2384fa4a5e370042078494
Gerson Soto <gersonsoto5622@gmail.com>
2022-08-29 20:00:50 -0500

    [Added] 02-name-types.ts

---
+++ src/clean-code/02-name-type.ts
@@ -0,0 +1,84 @@
+(() => {
+	// arreglo de temperaturas celsius
+	const arrayOfNums = [33.6, 12.34];
+	const temperaturesCelsius = [33.6, 12.34];
+
+	// Dirección ip del servidor
+	const ip = '123.123.123.123';
+	const ipServerDirection = '123.123.123.123';
+
+	// Listado de usuarios
+	const people = [
+		{ id: 1, email: 'fernando@google.com' },
+		{ id: 2, email: 'juan@google.com' },
+		{ id: 3, email: 'melissa@google.com' },
+	];
+
+	const users = [
+		{ id: 1, email: 'fernando@google.com' },
+		{ id: 2, email: 'juan@google.com' },
+		{ id: 3, email: 'melissa@google.com' },
+	];
+
+	// Listado de emails de los usuarios
+	const emails = people.map((u) => u.email);
+	const userEmails = people.map((u) => u.email);
+
+	// Variables booleanas de un video juego
+	const jump = false;
+	const canJump = false;
+	const run = true;
+	const canRun = true;
+	const noTieneItems = true;
+	const hastItems = true;
+	const loading = false;
+	const isLoading = false;
+
+	// Otros ejercicios
+	// tiempo inicial
+	const start = new Date().getTime();
+	const initialTime = new Date().getTime();
+	//....
+	// 3 doritos después
+	//...
+	// Tiempo al final
+	const end = new Date().getTime() - start;
+	const finalTime = new Date().getTime() - start;
+
+	// Funciones
+	// Obtiene los libros
+	function book() {
+		throw new Error('Function not implemented.');
+	}
+
+	function getBooks() {
+		throw new Error('Function not implemented.');
+	}
+
+	// obtiene libros desde un URL
+	function BooksUrl(u: string) {
+		throw new Error('Function not implemented.');
+	}
+
+	function getUrlBooks(u: string) {
+		throw new Error('Function not implemented.');
+	}
+
+	// obtiene el área de un cuadrado basado en sus lados
+	function areaCuadrado(s: number) {
+		throw new Error('Function not implemented.');
+	}
+
+	function getSquareArea(s: number) {
+		throw new Error('Function not implemented.');
+	}
+
+	// imprime el trabajo
+	function printJobIfJobIsActive() {
+		throw new Error('Function not implemented.');
+	}
+
+	function printJob() {
+		throw new Error('Function not implemented.');
+	}
+})();
